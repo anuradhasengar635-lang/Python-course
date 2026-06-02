874958fce1ad69aee372af9ac7a9f5c3c1b90fb1
anuradhasengar635-lang <anuradhasengar635@gmail.com>
2026-06-02 23:18:31 +0530

    lesson 1

---
+++ Module 1/Lesson1.py
@@ -0,0 +1,8 @@
+# This comment tell us my name.
+print("My name is vardaan")
+# This comment tells us that the what i am currently learning
+print("i am learning how to operate python")
+# This comment tells us my hobbies
+print(" My hobbies are playing Table tennis and learn coding.")
+# Performing and printing a simple calculation
+print("The sum of 2 and 3 is", 2 + 3)

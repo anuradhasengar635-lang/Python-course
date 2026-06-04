4890766de729ed2d52873a096a0429951231a283
anuradhasengar635-lang <anuradhasengar635@gmail.com>
2026-06-04 11:39:36 +0530

    lesson2

---
+++ Module 1/Lesson2/Index.py
@@ -0,0 +1,12 @@
+# Write a program to create different variables to store various values and print them
+# xyz=13
+# pi=3.14
+# name="vardaan"
+# print(xyz)
+# print(pi)
+# print(name)
+
+
+# WAP to print all the keywords
+import keyword
+print(keyword.kwlist)
